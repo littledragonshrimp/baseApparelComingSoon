62efef7855d0273ad9475e3ac01daf915a9932cd
Jordan <JDLChang@gmail.com>
2023-01-31 13:45:30 -0600

    initial commit

---
+++ actions.js
@@ -0,0 +1,16 @@
+// var x = document.getElementById("email").pattern;
+// 
+// function validateEmail = document.getElementById("email").pattern
+// 
+// 
+// FormData.addEventListener('submit',function(event){
+// })
+// 
+// console.log(email)
+// 
+// var email = document.getElementById('Email')
+// 
+// var error_message = document.createElement('div');
+// error_message.setAttribute("data-id", name);
+// error_message.classList.add('error-message');
+
