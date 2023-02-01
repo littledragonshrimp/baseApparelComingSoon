d9e16b720446467085843c5cd02052ee8a6d7e00
Jordan <JDLChang@gmail.com>
2023-02-01 12:01:24 -0600

    adds padding to input, adds js

--- actions.js
+++ actions.js
@@ -1,16 +1,31 @@
-// var x = document.getElementById("email").pattern;
-// 
-// function validateEmail = document.getElementById("email").pattern
-// 
-// 
-// FormData.addEventListener('submit',function(event){
-// })
-// 
-// console.log(email)
-// 
-// var email = document.getElementById('Email')
-// 
-// var error_message = document.createElement('div');
-// error_message.setAttribute("data-id", name);
-// error_message.classList.add('error-message');
+
+function emailChecker(emailInput) {
+    //re is just regular expression
+    var re = /\S+@\S+\.\S+/;
+    //test is just "make this boolean" so the result is true or false
+    return re.test(emailInput);
+  }
+      
+// This just adds email input
+let emailForm = document.getElementById("email-field");
+// console.log(emailForm);
+
+
+
+const validateEmail = () => {
+    console.log("validated email");
+
+    let emailValue = emailForm.value;
+
+    let result = emailChecker(emailValue);
+
+    if (result === false){
+        let errorMessage = document.getElementById("email-error-message");
+        errorMessage.className += ' show-error';
+    }
+}
+
+
+let submitButton = document.getElementById("email-button");
+submitButton.addEventListener('click', validateEmail);
 
