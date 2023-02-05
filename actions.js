2960428ade073cd72cad0672ec62a5c9de187826
Jordan <JDLChang@gmail.com>
2023-02-04 23:38:01 -0600

    adds javascript copy, button decoration

--- actions.js
+++ actions.js
@@ -1,31 +1,54 @@
 
-function emailChecker(emailInput) {
-    //re is just regular expression
-    var re = /\S+@\S+\.\S+/;
-    //test is just "make this boolean" so the result is true or false
-    return re.test(emailInput);
-  }
+// function emailChecker(emailInput) {
+//     //re is just regular expression
+//     var re = /\S+@\S+\.\S+/;
+//     //test is just "make this boolean" so the result is true or false
+//     return re.test(emailInput);
+//   }
       
-// This just adds email input
-let emailForm = document.getElementById("email-field");
-// console.log(emailForm);
+// // This just adds email input
+// let emailForm = document.getElementById("email-field");
+// // console.log(emailForm);
 
 
 
-const validateEmail = () => {
-    console.log("validated email");
+// const validateEmail = () => {
+//     console.log("validated email");
 
-    let emailValue = emailForm.value;
+//     let emailValue = emailForm.value;
 
-    let result = emailChecker(emailValue);
+//     let result = emailChecker(emailValue);
 
-    if (result === false){
-        let errorMessage = document.getElementById("email-error-message");
-        errorMessage.className += ' show-error';
-    }
+//     if (result === false){
+//         let errorMessage = document.getElementById("email-error-message");
+//         errorMessage.className += ' show-error';
+//     }
+// }
+
+
+// let submitButton = document.getElementById("email-button");
+// submitButton.addEventListener('click', validateEmail);
+
+
+
+const button = document.querySelector("button#send");
+const errorIcon = document.querySelector("#form-email>img");
+const errorMsg = document.querySelector("#form-email>span");
+const email = document.querySelector("input#email-field");
+
+function Error(){
+    errorIcon.classList.contains("hidden") && errorIcon.classList.remove("hidden");
+    errorMsg.classList.contains("hidden") && errorMsg.classList.remove("hidden");
 }
 
+function Ok(){
+    !errorIcon.classList.contains("hidden") && errorIcon.classList.add("hidden");
+    !errorMsg.classList.contains("hidden") && errorMsg.classList.add("hidden");
+}
 
-let submitButton = document.getElementById("email-button");
-submitButton.addEventListener('click', validateEmail);
+function send(e){
+    e.preventDefault();
+    email.checkValidity() ? Ok() : Error()
+}
 
+button.addEventListener('click',send);
